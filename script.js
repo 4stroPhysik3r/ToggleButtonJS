6679ac701bd641d17a56f1fa002c826ec165dd04
Alex <alex.embi@gmail.com>
2023-06-21 16:17:34 +0300

    ToggleButton

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleBold() {
+  const el = document.querySelector("p");
+    el.classList.toggle("bold");
+}
+
+function toggleItalic() {
+  const el = document.querySelector("p");
+    el.classList.toggle("italic");
+}
+
+function toggleUnderline() {
+    const el = document.querySelector("p");
+        el.classList.toggle("underline");
+    }
+
+function toggleHighlight() {
+    const el = document.querySelector("p");
+        el.classList.toggle("highlight");
+    }
